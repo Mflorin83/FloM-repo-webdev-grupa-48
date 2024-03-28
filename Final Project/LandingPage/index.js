d5c6d1e570314509c79a1c9d1faa008e51c292e2
Mflorin83 <florinionut.mihai@gmail.com>
2024-03-28 16:34:40 +0200

    lucru la meniu

---
+++ Final Project/LandingPage/index.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var menuIcon = document.getElementsByClassName("site-nav-mobile")[0];
+    var mobileNav = document.getElementsByClassName("site-nav")[0];
+    var menuIconOpen = document.getElementsByClassName("menu-open")[0];
+    var menuIconClose = document.getElementsByClassName("menu-close")[0];
+    menuIcon.addEventListener("click", mobileMenu);
+    function mobileMenu() {
+        if (mobileNav.style.display === "flex") {
+            mobileNav.style.display = "none";
+            menuIconOpen.style.display = "flex";
+            menuIconClose.style.display = "none";
+        } else {
+            mobileNav.style.display = "flex";
+            menuIconClose.style.display = "flex";
+            menuIconOpen.style.display = "none";
+        }
+    }
+})
