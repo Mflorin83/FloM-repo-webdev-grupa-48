b5d355f182cced673bf7528a2f2c77c7804801b4
Mflorin83 <florinionut.mihai@gmail.com>
2024-05-18 15:22:47 +0300

    configureg bootstrap with css and js

--- Angular/AppNew/src/app/app.component.ts
+++ Angular/AppNew/src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, NgbModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
