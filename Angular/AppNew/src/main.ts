0aad50605c3507d13d40a6857432ded24fefa3c1
Mflorin83 <florinionut.mihai@gmail.com>
2024-05-19 18:01:34 +0300

    finished merge

--- Angular/AppNew/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
