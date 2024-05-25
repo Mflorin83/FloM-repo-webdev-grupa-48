e3b93aa0560663f20219ce1975bbc3d0e06487a8
Mflorin83 <florinionut.mihai@gmail.com>
2024-05-25 22:18:29 +0300

    getfromAPIsucces

--- Final Project/JS/main.js
+++ Final Project/JS/main.js
@@ -38,3 +38,33 @@ export function initializePopovers(selector) {
     const popoverTriggerList = [].slice.call(document.querySelectorAll(selector));
     const popoverList = popoverTriggerList.map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
 }
+
+
+export function getUserFromApi() {
+    const API_URL = "http://localhost:5500/Users";
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(data => {
+            const userTableBody = document.getElementById('injectedContent');
+            data.forEach(user => {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                            <td><input type="text" class="form-control form-control-sm p-1 fw-light fs-6" value="${user.id}" disabled></td>
+                            <td><input type="text" class="form-control form-control-sm p-1 fw-light fs-6" value="${user.surname}" disabled></td>
+                            <td><input type="text" class="form-control form-control-sm p-1 fw-light fs-6" value="${user.name}" disabled></td>
+                            <td><input type="text" class="form-control form-control-sm p-1 fw-light fs-6" value="${user.phone}" disabled></td>
+                            <td><input type="email" class="form-control form-control-sm p-1 fw-light fs-6" value="${user.email}" disabled></td>
+                            <td><input type="password" class="form-control form-control-sm p-1 fw-light fs-6" value="${user.password}" disabled></td>
+                            <td><input type="text" class="form-control form-control-sm p-1 fw-light fs-6" value="${user.role || 'user'}" disabled></td>
+                            <td>
+                            <button type="button" class="btn btn-primary btn-sm" onclick="toggleEdit('${user.id}', true)">Edit</button>
+                            <button type="button" class="btn btn-success btn-sm" onclick="saveClient('${user.id}')" style="display:none;">Save</button>
+                            <button type="button" class="btn btn-secondary btn-sm" onclick="toggleEdit('${user.id}', false)" style="display:none;">Cancel</button>
+                            <button type="button" class="btn btn-danger btn-sm" onclick="deleteClient('${user.id}')">Delete</button>
+                            </td>
+                        `;
+                userTableBody.appendChild(row);
+            });
+        })
+        .catch(error => console.error('Error fetching user data:', error));
+}
